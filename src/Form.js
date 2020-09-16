f005f8c27b9495775cb38dc2fa0ed971f941c8c1
itortouch <46375015+itortouch@users.noreply.github.com>
2020-09-16 19:48:13 +0200

    cambio url post region

--- src/Form.js
+++ src/Form.js
@@ -24,7 +24,7 @@ export default class Form extends Component {
     event.preventDefault();
     const { name, message } = this.state;
     await axios.post(
-      'https://i1xsjzkri4.execute-api.us-east-1.amazonaws.com/default/serverlessAppFunction',
+      'https://0zgmq05pw7.execute-api.eu-west-3.amazonaws.com/default/serverlessAppFunction',
       { key1: `${name}, ${message}` }
     );
   }
